5d9bf418f35cca6fec9c8ebc5dbae3e06232633e
Alex <alexonej@gmail.com>
2017-02-12 19:07:32 -0800

    iframes for youtube vids

--- src/VideoListItem.jsx
+++ src/VideoListItem.jsx
@@ -4,12 +4,20 @@ class VideoListItem extends Component {
     constructor(props) {
         super(props);
     }
+
+    
     
     
     render() {
+        const imageUrl = this.props.video.snippet.thumbnails.default.url;
+        const videoId = this.props.video.id.videoId; 
+        const url = `https://www.youtube.com/embed/${videoId}`;
+        
         return (
             <li>
                 <div>{this.props.video.snippet.title}</div>
+                <div><img src={imageUrl}/></div>
+                <iframe type="text/html" src={url}></iframe>
             </li>
         );
     }
